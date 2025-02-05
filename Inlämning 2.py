fc0f0989ee9256a1fb3bdd4d7d180c830cc02e8f
jack8@outlook.com <jack-81623@outlook.com>
2025-02-05 17:41:15 +0100

    Add instruktioner Inlämningsuppgift 1.py Delete anteckningar från lektionen

---
+++ Inlämning 2.py
@@ -0,0 +1,34 @@
+from tkinter import messagebox, simpledialog
+
+
+#Antal minuter
+Arra = [10,20,30,35]
+
+#import
+minute = 60
+calories = 4.2
+User_input = int(messagebox.askint("skriv in hur många min du har sprungit:"))
+#loop
+for number in array:
+    Svar= simpledialog.askint("Fråga","Hur många minuter har du sprungit ")
+    calculation= calories * minute * user_input
+    print(f"Du har bränt såhär många kalorier: " )
+
+#Message
+ b
+
+"""
+while (keep_going):
+    hours = simpledialog.askfloat("hours", "how many hours?")
+    payrate = simpledialog.askfloat("payment", "what is your hourly payrate")
+
+    pay = hours*payrate
+
+    #message
+    message =f" your payment will be {pay:.2f} based on the hours worked which was {hours} and the payrate if {payrate}"
+    messagebox.showinfo(f"your payment {pay:.2f}", message)
+
+
+    keep_going=messagebox.askyesno("continue", "do you want to continue?")
+"""
+
