afc1739a4f04fbc1163530612cdd1a7044388e9a
RavenMaker <82211266+RavenMaker@users.noreply.github.com>
2025-02-10 15:32:24 +0100

    Uppdated 0.1.3

--- Labb3.py
+++ Labb3.py
@@ -34,7 +34,7 @@ def main():
             if choice == MenyVal.Uppgift_1.value:
                 messagebox.showinfo("Uppgift 1","Du har valt Uppgift 1 som handlar om The kilometer converter problem!")
                 Uppgift = simpledialog.askfloat("Distance", "Hur långt avstånd i km har du kört?")
-                messagebox.showinfo("The Kilometer Converter Problem",f"Du har selectat hur många kilometer du valt som är {Uppgift} och uträkningen för detta till miles är detta:\n{Uppgift1.kilometer(Uppgift):.2f}={Uppgift}*0.6214")
+                messagebox.showinfo("The Kilometer Converter Problem",f"Du har selectat hur många kilometer du valt som är {Uppgift} och uträkningen för detta till miles är detta:\n{Uppgift1.kilometer(Uppgift,0.6214):.2f}={Uppgift}*0.6214")
             elif choice == MenyVal.Uppgift_2.value:
                 Uppgift = messagebox.askyesno("Uppgift 2","Är du säker på att du vill starta Uppgift 2?")
 
--- Labb3_Uppgift1.py
+++ Labb3_Uppgift1.py
@@ -1,2 +1,2 @@
-def kilometer(kilometer=0.0,distens=0.6214):
-    return kilometer*distens
+def kilometer(km=0.0,faktorn=0.0):
+    return km*faktorn
