bd98d7bec0f0be80c937101aa4c9d050efa9c638
RavenMaker <82211266+RavenMaker@users.noreply.github.com>
2025-02-12 11:42:57 +0100

    Labb4 update 0.2.1

--- Labb4_Uppgift7.py
+++ Labb4_Uppgift7.py
@@ -1,4 +1,4 @@
 import random
 with open('numbers.txt','w',encoding='utf-8') as number_file:
-    for Save_rounds in range(int(input("skriv in hur många rader du vill att datorn ska spara av random sifror: "))):
-       number_file.write(str(random.randint(1, 500)) + '\n')
+    for Save_rounds in range(int(input("skriv in hur många rader du vill att datorn ska spara av random sifror: "))):#frågar användaren hur många rader du vill spara
+       number_file.write(str(random.randint(1, 500)) + '\n')#kör så många gånger användaren har laggt till att loopa igenom
--- Labb4_Uppgift8.py
+++ Labb4_Uppgift8.py
@@ -1,7 +1,7 @@
 with open('numbers.txt','r',encoding='utf-8') as fil:
-    total_Sum=0
-    rad=0
+    total_Sum=0#variable för inne i filen
+    rad=0#variable för inne i filen
     for fil_number in fil:
-        rad+=1
-        total_Sum+=int(fil_number)
-    print(f"totala summan: {total_Sum}\nså här många rader är sparade: {rad}")
+        rad+=1 #lägger till en rad som den har läst in
+        total_Sum+=int(fil_number) #räknar ihop till en total summa
+    print(f"totala summan: {total_Sum}\nså här många rader är sparade: {rad}")#srkiver ut alla svaren på skärmen
