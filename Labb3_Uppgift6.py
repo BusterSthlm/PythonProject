c5b9b7eab49f7f475d767585c88d61c2c21eb40a
jack8@outlook.com <jack-81623@outlook.com>
2025-02-13 20:06:26 +0100

    Updated 0.6.10

--- Labb3_Uppgift6.py
+++ Labb3_Uppgift6.py
@@ -2,9 +2,10 @@ from tkinter import messagebox, simpledialog
 
 #Uppgift 6 Calories from Fat and Carbohydrates
 
+# Hämtar gram fett från användaren
 def input_gram_fat():
     return simpledialog.askfloat("fråga", "Hur många gram fett har du blivit av med på senaste året")
-
+# Hämtar gram kolhydrater från användaren
 def input_gram_carbonhydrate():
     return simpledialog.askfloat("fråga","Hur många gram kolhydrater har du blivit av med på senaste året")
 
@@ -12,10 +13,11 @@ def input_gram_carbonhydrate():
 def calories_from_fat(gram_fat):
     return gram_fat * 9
 
-# beräkna kalorier från kolhydrater
+# Beräkna kalorier från kolhydrater
 def calories_from_carbonhydrates(gram_carbonhydrate):
     return gram_carbonhydrate * 9
 
+# visa svaret
 def visar_svar(gram_fat,gram_carbonhydrate):
     all_cals = calories_from_fat(gram_fat)
     all_carbs = calories_from_carbonhydrates(gram_carbonhydrate)
--- Labb3_Uppgift7.py
+++ Labb3_Uppgift7.py
@@ -11,40 +11,40 @@ and Class C seats cost $10.
 Write a program that asks how many tickets for each class of seats
 were sold, then displays the amount of income generated from ticket sales.
 """
-total_value = 0  # Defining total_value at the beginning
+total_value = 0  # Totalen ska börja på 0
 
 
+# enum kategori säten A,B,C
 class SeatingCost(Enum):
     CLASS_A = 20
     CLASS_B = 15
     CLASS_C = 10
 
 
-# Function to ask how many tickets were sold for each class
+# Fråga hur många biljetter som har sålts.
 def tickets():
     class_a_tickets = simpledialog.askinteger("Fråga", "Hur många biljetter har kategori A sålt?")
     class_b_tickets = simpledialog.askinteger("Fråga", "Hur många biljetter har kategori B sålt??")
     class_c_tickets = simpledialog.askinteger("Fråga", "Hur många biljetter har kategori C  sålt??")
     return class_a_tickets, class_b_tickets, class_c_tickets
 
-# Function to calculate the total income from ticket sales
+# Beräkning av total värdet.
 def sold_tickets(class_a_tickets, class_b_tickets, class_c_tickets):
     global total_value  # Make sure to use the global variable `total_value`
 
-    # Calculate total income for each class
+    # Uträkning
     total_value += class_a_tickets * SeatingCost.CLASS_A.value
     total_value += class_b_tickets * SeatingCost.CLASS_B.value
     total_value += class_c_tickets * SeatingCost.CLASS_C.value
 
 
-# Main program execution
-def main():
-    class_a_tickets, class_b_tickets, class_c_tickets = tickets()  # Get the number of tickets sold
-    sold_tickets(class_a_tickets, class_b_tickets, class_c_tickets)  # Calculate total income
+def run_program():
+    class_a_tickets, class_b_tickets, class_c_tickets = tickets()  # Hämtar antal nummer sålda
+    sold_tickets(class_a_tickets, class_b_tickets, class_c_tickets)  # Antal sålda
 
     # Display the total income
     messagebox.showinfo("Inkomst", f"Total income from ticket sales: ${total_value}")
 
 
 if __name__ == '__main__':
-    main()
+    run_program()
