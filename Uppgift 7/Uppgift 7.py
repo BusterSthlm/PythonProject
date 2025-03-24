41cbb8fc98f6cde505ccbec2efaecf1078775c64
jack8@outlook.com <jack-81623@outlook.com>
2025-03-24 19:13:46 +0100

    Uppgift 7 functions

--- Uppgift 7/Uppgift 7.py
+++ Uppgift 7/Uppgift 7.py
@@ -1,9 +1,46 @@
-########## Beskrivning ##########
-"""
-Typografihanterare: Bygg en app för att organisera och visa olika typsnitt. Skapa
-upp, läs, uppdatera och ta bort typsnitt.
 
 """
+BESKRIVNING AV UPPGIFT 7
+Typografihanterare:
+ Bygg en app för att organisera och visa olika typsnitt.
+    Skapa upp, Läs,uppdatera , ta bort typsnitt
 
 
-########## Beskrivning ##########
+"""
+
+import tkinter as tk
+import json
+import os
+from tkinter import messagebox, simpledialog
+
+
+
+def main():
+    fonts = [font1,font2, font3]
+
+def
+
+    pass
+
+
+
+# Skapa upp - Create
+def add_new_font():
+    font.append()
+    pass
+
+# Sök - read
+def search_font():
+    pass
+
+# Redigera
+def edit_font():
+    pass
+
+# Ta bort
+def remove_font():
+    pass
+
+
+
+# spara JSon i Directionary Som tidigare.
