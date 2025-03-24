3ce24611a01f5d10945d56e924b8f9f9e57dd85b
jack8@outlook.com <jack-81623@outlook.com>
2025-03-24 20:12:49 +0100

    Uppgift 7 functions

--- Uppgift 7/Uppgift 7.py
+++ Uppgift 7/Uppgift 7.py
@@ -13,33 +13,49 @@ import json
 import os
 from tkinter import messagebox, simpledialog
 
-
+from Labrationer.Labb3 import print_meny
+# Global
+# Global
+fontfamily =[]
 
 def main():
-    fonts = [font1,font2, font3]
+    fonts_list = [Helvetica,Verdana,Times_New_Roman]
 
-def
+
+
+def get_menu_text(root):
+    Meny = ["1. Skapa typsnitt",
+             "2. Sök efter typsnitt",
+             "3. Redigera typsnitt",
+             "4. Ta bort typsnitt",
+             "5. Avsluta programet"]
 
     pass
 
-
-
 # Skapa upp - Create
 def add_new_font():
+
     font.append()
+    print("Din font har lagts till i listan")
     pass
 
 # Sök - read
 def search_font():
-    pass
+        items = groceries
+        item_name = str(input("Vilken font söker du? "))
+        for item in items: # Söker igenom lista
+            if name.get() == item.get_name():
+                print(item)
+
 
 # Redigera
 def edit_font():
+
     pass
 
 # Ta bort
 def remove_font():
-    pass
+    fonts_list.clear()
 
 
 
