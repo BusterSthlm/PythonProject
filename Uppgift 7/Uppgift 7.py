7578140e44fec58ff88628153069584c7c724393
jack8@outlook.com <jack-81623@outlook.com>
2025-03-24 18:35:29 +0100

    Uppgift 7

---
+++ Uppgift 7/Uppgift 7.py
@@ -0,0 +1,9 @@
+########## Beskrivning ##########
+"""
+Typografihanterare: Bygg en app för att organisera och visa olika typsnitt. Skapa
+upp, läs, uppdatera och ta bort typsnitt.
+
+"""
+
+
+########## Beskrivning ##########
