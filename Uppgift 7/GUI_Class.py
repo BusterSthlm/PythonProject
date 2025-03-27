8249ee35b2e4f9b33398c66d56659602a6b135da
RavenMaker <82211266+RavenMaker@users.noreply.github.com>
2025-03-27 13:34:54 +0100

    uppgift 7 Gui version 2.0

--- Uppgift 7/GUI_Class.py
+++ Uppgift 7/GUI_Class.py
@@ -2,15 +2,29 @@ import tkinter as tk
 class Gui_interface:
     def __init__(self):
         self.__root = tk.Tk()
-        self.__size = "600x500"
+        self.__ID = "typsnitt"
+        self.__IDNumber = 3
+        self.__dict = {"typsnitt1":"Helvetica","typsnitt2": "Verdana"}
 
+    def get_Id(self):
+        return self.__ID
 
-    def get_size(self):
-        return self.__size
+    def get_IdNumber(self):
+        return self.__IDNumber
+    def set_IDNumber(self,number):
+        self.__IDNumber+=number
 
-    def get_name(self):
-        return self.__name
+    def get_dict(self):
+        return self.__dict
 
+    def set_dict(self,dicts):
+        self.__dict = dicts
 
     def get_root(self):
         return self.__root
+
+    def __str__(self):
+        message =""
+        for messages in self.__dict.values():
+            message=message+f"{messages} finns sparad i dictanary!\n{"-"*36}\n"
+        return f"{message}"
--- Uppgift 7/Main.py
+++ Uppgift 7/Main.py
@@ -1,103 +1,197 @@
 import tkinter as tk
 import GUI_Class as gui
-import json
-import os
 
 # Global
-fontfamily = ["Helvetica", "Verdana", "Times New Roman"]
-root_size = gui.Gui_interface()
-MainFrame = tk.Frame(root_size.get_root(), padx=5,pady=10)  # framen gör så det blir inget att flyta plats med varandra
+font_root = gui.Gui_interface()
+MainFrame = tk.Frame(font_root.get_root(), padx=5,pady=10)  # framen gör så det blir inget att flyta plats med varandra
 # Global
 
+###############################################################################################################
+
+# Main funktionerna
 def main():
     GUI_interface()
-    root_size.get_root().mainloop()
+    font_root.get_root().mainloop()
 
 def GUI_interface():
-    root_size.get_root().title("Uppgift 7")
-    root_size.get_root().config(background="White")
-    root_size.get_root().geometry(root_size.get_size())
-    MainFrame.config(background="green", width=600, height=500)
+    font_root.get_root().title("Uppgift 7")
+    font_root.get_root().config(background="#6082B6")
+    font_root.get_root().geometry("680x500")
+
+    MainFrame.config(background="#6082B6", width=640, height=500)
     MainFrame.grid(row=0, column=0)
+
     buttonFrame = tk.Frame(MainFrame)
-    buttonFrame.config(background="blue",width=185,height=480)
+    buttonFrame.config(background="Steel Blue",width=185,height=480,borderwidth=2,relief="sunken")
     buttonFrame.grid(row=2,column=1,padx=5)
+
     MainText = tk.Label(buttonFrame)
-    MainText.config(text="Typsnitt",font=10)
+    MainText.config(text="Typsnitt",font=10,background="Steel Blue")
     MainText.grid(row=0, column=2,padx=5,pady=10)
+
     Buttons(buttonFrame)
-    functionFrame("yellow")
+
+    function_Frame_placement=functionFrame("#6082B6")
+    function_Frame_placement.grid(padx=10)
 
 def Buttons(root):
-    comandsPrompt=[add_new_font,seach_fonts,edit_font,remove_font,exit]
-    namePrompt=["Nytt typsnitt\n(skapa Upp)","Sök typsnitt\n(läs)","Redigera typsnitt\n(uppdatera)","Ta bort\n(radera)","Avsluta programet\n(stänga av)"]
+    comandsPrompt=[add_new_font,print_fonts,edit_font,remove_font,exit]
+    namePrompt=["Nytt typsnitt\n(skapa Upp)","skriv ut all typsnitt\n(läs)","Redigera typsnitt\n(uppdatera)","Ta bort typsnitt\n(radera)","Avsluta programet\n(stänga av)"]
     # Lägg till ,command=comandsPrompt[i-1] när färdig med funktonerna
     for i in range(1, 6):  # skapar knappar och soclar ut också komando för varje funtion som fins i listan ovanför
         button = tk.Button(root, text=namePrompt[i-1], width=14)
         button.grid(row=i, column=2, pady=13, padx=40)
-        button.configure(command=comandsPrompt[i - 1])
+        button.configure(command=comandsPrompt[i - 1],background="#6082B6", borderwidth=2,relief="ridge")
 
 def functionFrame(colors):
-    Frame = tk.Frame(MainFrame)
-    Frame.config(background=colors, width=385, height=480)
+    Frame = tk.Frame(MainFrame)#skapar frames som används hela applications gång
+    Frame.config(background=colors, width=440, height=480)
     Frame.grid(row=2, column=3, padx=5)
     return Frame
 
 def descriptions_text(text_Insering,frame,colors):
     count=0
-    for text in text_Insering:
+    for text in text_Insering:#skapar labels som lägg tills när det funktioner vill ha det
+        #skaper positioner för texten när vi lägger till dom
         text_intreduction = tk.Label(frame, text=text)
         text_intreduction.config(background=colors)
-        text_intreduction.grid(row=count, column=2-count,padx=5,pady=5)
+        if count == 0:
+            text_intreduction.grid(row=count, column=2-count,padx=5,pady=5)
+        else:
+            text_intreduction.grid(row=count, column=1, padx=5, pady=5)
         count+=1
 
 def textArea(frame):
-    text_contener = tk.Text(frame, width=36, height=10, borderwidth=1, relief="solid")
+    #text area där sickas in resultatet från vi sickar ut till användaren
+    text_contener = tk.Text(frame, width=36, height=10, borderwidth=1, relief="solid")#skapar en text area som vi kan använda oss av i funktionerna
     text_contener.grid(row=5, column=2, pady=10, padx=10)
     return text_contener
+###############################################################################################################
+
+
+
 
 # Skapa upp - Create
 def add_new_font():
-    frame=functionFrame("red")
-    descriptions_text(["Lägg till fonts","Font namn:"],frame,"red")
-    text_contener=textArea(frame)
+    #första grejerna som läggs till i början
+    functionFrame("#6082B6") #skapar frame för att gömma tidigare funktioner
+    frame=functionFrame("Steel blue") #skapar frame för denna funktionen
+    frame.config(borderwidth=2, relief="sunken")
+    descriptions_text(["Lägg till typsnitt","typsnitt namn:"],frame,"Steel blue")    #skappar label texter till funktionen
+    text_contener=textArea(frame)#skapar en text area för all out put
+    text_contener.insert(1.0,"Skriv in ett typsnitt namn!\n")
+    #skapar entrys och knappar
     add_entry=tk.Entry(frame,width=40)
     add_entry.config(borderwidth=1, relief="solid")
     add_entry.grid(row=1,column=2,pady=5,padx=5)
-    add_fonts=tk.Button(frame,text="Lägg till",command=lambda : add_font(add_entry,text_contener))
-    add_fonts.config(borderwidth=2,relief="solid")
+
+    add_fonts=tk.Button(frame,text="Lägg till",command=lambda : add_font(add_entry.get(),text_contener))#knapp som sickar oss vidare till funktionen nedan för
+    add_fonts.config(background="#6082B6", borderwidth=2,relief="ridge")
     add_fonts.grid(row=2,column=2,pady=5,padx=5)
 
 
 def add_font(new_font,text_contener):
-    fontfamily.append(new_font.get())#behöver en text här i ()
-    text_contener.insert(1.0,f"Du har lagt till {new_font.get()} i listan!\n")
+    font_dict=font_root.get_dict()
+    cheacking_dict_font=False
+    for font_names in font_root.get_dict().values():
+        if font_names == new_font:
+            cheacking_dict_font=True
+    if cheacking_dict_font:
+        text_contener.insert(1.0, f"{new_font} finns redan i listan!\n")
+    else:
+        IdKey=f"{font_root.get_IdNumber()}"
+        font_root.set_IDNumber(1)
+        key=f"{font_root.get_Id()+IdKey}"
+        font_dict[key]=new_font
+        font_root.set_dict(font_dict)
+        text_contener.insert(1.0,f"Du har lagt till {new_font} i listan!\n")
 
-def seach_fonts():
-    frame=functionFrame("purple")
-    descriptions_text(["Sök efter fonts","söker efter:"],frame,"purple")
-    text_contener = textArea(frame)
-    Seach_entry=tk.Entry(frame,width=40)
-    Seach_entry.config(borderwidth=1, relief="solid")
-    Seach_entry.grid(row=1,column=2,pady=5,padx=5)
-    Seach_fonts=tk.Button(frame,text="Lägg till",command=lambda : seach_font(Seach_entry,text_contener))
-    Seach_fonts.config(borderwidth=2,relief="solid")
-    Seach_fonts.grid(row=2,column=2,pady=5,padx=5)
+###############################################################################################################
 
-def seach_font(name,text_contener):
-    for fonts in fontfamily:  # Söker igenom lista
-        if name.get() == fonts:
-            text_contener.insert(1.0, f"{name.get()} fonten\nfins här i listan!\n")
 
+
+
+# Skriv ut - Read
+def print_fonts():
+    # första grejerna som läggs till i början
+    functionFrame("#6082B6")#skapar frame för att gömma tidigare funktioner
+    frame=functionFrame("Steel blue")#skapar frame för denna funktionen
+    frame.config(borderwidth=2, relief="sunken")
+    descriptions_text(["Skriv ut all typsnitt"],frame,"Steel blue")#skappar label texter till funktionen
+    text_contener = textArea(frame) #skapar en text area för all out put
+    text_contener.insert(1.0, str(font_root))#Sickar ut all info om fonterna
+###############################################################################################################
+
+
+
+# Edit - Update
 def edit_font():
-    functionFrame("pink")
+    # första grejerna som läggs till i början
+    functionFrame("#6082B6")#skapar frame för att gömma tidigare funktioner
+    frame=functionFrame("Steel blue")#skapar frame för denna funktionen
+    frame.config(borderwidth=2, relief="sunken")
+    descriptions_text(["Redigera en typsnitt", "Gammla typsnitt:","Nytt typsnitt:"], frame, "Steel blue")#skappar label texter till funktionen
+    text_contener = textArea(frame)#skapar en text area för all out put
+    text_contener.insert(1.0, "Redigera ett typsnitt namn!\n")
+    #skapar entrys och knappar
+    oldfont_entry = tk.Entry(frame, width=40)
+    oldfont_entry.config(borderwidth=1, relief="solid")
+    oldfont_entry.grid(row=1, column=2, pady=5, padx=5)
 
+    newfont_entry = tk.Entry(frame, width=40)
+    newfont_entry.config(borderwidth=1, relief="solid")
+    newfont_entry.grid(row=2, column=2, pady=5, padx=5)
+
+    edit_fonts_button = tk.Button(frame, text="Edit typsnitt", command=lambda: editing_font(newfont_entry.get(),oldfont_entry.get(), text_contener))
+    edit_fonts_button.config(background="#6082B6", borderwidth=2,relief="ridge")
+    edit_fonts_button.grid(row=3, column=2, pady=5, padx=5)
+
+def editing_font(new_name,old_name,text_contener):
+    edit_cheack=True
+    for font_keys,font_names in font_root.get_dict().items():# Om listan finns byter den ut namnet med ett nytt
+        if font_names == old_name:
+            font_dict=font_root.get_dict()
+            font_dict[font_keys]=new_name
+            text_contener.insert(1.0,f'typsnitt {old_name} ändrad till {new_name}!\n')
+            edit_cheack=False
+    if edit_cheack:
+        text_contener.insert(1.0,f'typsnitt {old_name} finns inte i dictanaryin!\n')
+###############################################################################################################
+
+
+
+# Ta bort - Delete
 def remove_font():
-    functionFrame("gray")
-    fontfamily.clear()
+    functionFrame("#6082B6")#skapar frame för att gömma tidigare funktioner
+    frame=functionFrame("Steel blue")#skapar frame för denna funktionen
+    frame.config(borderwidth=2, relief="sunken")
+    descriptions_text(["Ta bort en typsnitt", "typsnitt Namn:"], frame, "Steel blue")#skappar label texter till funktionen
+    text_contener = textArea(frame)#skapar en text area för all out put
+    text_contener.insert(1.0, "Ta bort ett typsnitt namn!\n")
+    # skapar entrys och knappar
+    entry_remove_font = tk.Entry(frame,width=40)
+    entry_remove_font.config(borderwidth=1, relief="solid")
+    entry_remove_font.grid(row=1, column=2, pady=5, padx=5)
 
+    remove_fonts_button = tk.Button(frame, text="Ta bort",command=lambda: removing_font(entry_remove_font.get(),text_contener))
+    remove_fonts_button.config(background="#6082B6", borderwidth=2,relief="ridge")
+    remove_fonts_button.grid(row=3, column=2, pady=5, padx=5)
 
-
+def removing_font(remove_font,text_contener):
+    remove_cheack=True
+    remove_key=""
+    for remove_font_key,remove_font_name in font_root.get_dict().items():
+        if remove_font_name == remove_font:
+            remove_key=remove_font_key
+            text_contener.insert(1.0,f'Typsnitt {remove_font} har tagits bort!\n')
+            remove_cheack=False
+    if remove_cheack:
+        text_contener.insert(1.0,f'Typsnitt {remove_font} finns inte i listan!\n')
+    else:
+        font_dict = font_root.get_dict()
+        font_dict.pop(remove_key)
+        font_root.set_IDNumber(-1)
+###############################################################################################################
 
 
 if __name__ == '__main__':
