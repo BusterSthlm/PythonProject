05603896b7d412b6dbeb7632eb67d5b61093ac23
jack8@outlook.com <jack-81623@outlook.com>
2025-03-27 15:25:24 +0100

    Uppgift 7 functions

--- Uppgift 7/GUI_Class.py
+++ Uppgift 7/GUI_Class.py
@@ -5,26 +5,26 @@ class Gui_interface:
         self.__ID = "typsnitt"
         self.__IDNumber = 3
         self.__dict = {"typsnitt1":"Helvetica","typsnitt2": "Verdana"}
-
+# ID För typsnittet
     def get_Id(self):
         return self.__ID
-
+    #Nummer kopplat till iD. Varja font får var sitt nummer
     def get_IdNumber(self):
         return self.__IDNumber
     def set_IDNumber(self,number):
         self.__IDNumber+=number
-
+    #skapa en Dictionatary
     def get_dict(self):
         return self.__dict
-
+    #Skriv till dict
     def set_dict(self,dicts):
         self.__dict = dicts
-
+    # Skapar Root GUI
     def get_root(self):
         return self.__root
 
     def __str__(self):
         message =""
-        for messages in self.__dict.values():
+        for messages in self.__dict.values(): #loopar igenom dictonary o skapar en sträng
             message=message+f"{messages} finns sparad i dictanary!\n{"-"*36}\n"
         return f"{message}"
